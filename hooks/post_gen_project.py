96078a48c625985fb72a48ca27c945525494111b
Austin Ziegler <austin@zieglers.ca>
2016-11-30 23:54:30 -0500

    Change the hook to a shell script and commit

--- hooks/post_gen_project.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import subprocess
-
-subprocess.Popen([ "/bin/bash", "-c", "git init" ])
---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+git init
+git add .
+git commit -m "Cookiecutter generated initial Elixir project: {{ cookiecutter.project_slug }}"
